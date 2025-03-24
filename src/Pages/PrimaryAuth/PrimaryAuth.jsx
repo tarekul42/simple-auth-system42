e7f9cc7fa544e46c2723f4c98a11055b231fa0f8
tarekul42 <tarekulrifat205@gmail.com>
2025-03-24 19:42:39 +0600

    basic design done of Primary Authentication page

--- src/Pages/PrimaryAuth/PrimaryAuth.jsx
+++ src/Pages/PrimaryAuth/PrimaryAuth.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import GoogleLogin from "../../Components/GoogleLogin/GoogleLogin";
+import GithubLogin from "../../Components/GithubLogin/GithubLogin";
 
 const PrimaryAuth = () => {
-    return (
-        <div>
-            this is primary auth
+  return (
+    <>
+      <div className="bg-base-200">
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="hero">
+          <div className="hero-content">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <fieldset className="fieldset">
+                  <label className="fieldset-label">Email</label>
+                  <input
+                    type="email"
+                    className="input"
+                    placeholder="Enter your email here"
+                  />
+                  <button className="btn btn-neutral mt-4">Check</button>
+                </fieldset>
+                <div className="divider">OR</div>
+                <div className="flex flex-col gap-4">
+                  <GoogleLogin />
+                  <GithubLogin />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      </div>
+    </>
+  );
 };
 
 export default PrimaryAuth;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.jsx";
+import Login from "./Pages/Login/Login.jsx";
+import SignUp from "./Pages/SignUp/SignUp.jsx";
+import PrimaryAuth from "./Pages/PrimaryAuth/PrimaryAuth.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<PrimaryAuth />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
