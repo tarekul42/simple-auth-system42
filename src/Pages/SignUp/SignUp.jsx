b8351226820015bf1be645264a2750eaae1fd302
tarekul42 <tarekulrifat205@gmail.com>
2025-03-25 13:46:19 +0600

    signup page design almost done

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React from "react";
 
 const SignUp = () => {
-    return (
-        <div>
-            this is signup page
+
+    const handleSignUp = e =>{
+        e.preventDefault();
+
+        const inputField = e.target;
+        const name = inputField.name.value;
+        const email = inputField.email.value;
+        const password = inputField.password.value;
+        const confirmPassword = inputField.confirmPassword.value;
+
+        console.log(name, email, password, confirmPassword)
+    }
+
+  return (
+    <>
+      <div className="bg-base-200">
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="hero">
+            <div className="hero-content flex flex-col">
+              <h1 className="text-3xl font-bold">Register now!</h1>
+              <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                  <form onSubmit={handleSignUp}>
+                    <fieldset className="fieldset">
+                      {/* <label className="fieldset-label">Name</label> */}
+                      <input
+                        type="name"
+                        name="name"
+                        className="input"
+                        placeholder="Enter your name here"
+                      />
+                      {/* <label className="fieldset-label">Email</label> */}
+                      <input
+                        type="email"
+                        name="email"
+                        className="input my-4"
+                        placeholder="Enter your email here"
+                      />
+                      {/* <label className="fieldset-label">Password</label> */}
+                      <input
+                        type="password"
+                        name="password"
+                        className="input"
+                        placeholder="Make a strong password"
+                      />
+                      {/* <label className="fieldset-label">Confirm Password</label> */}
+                      <input
+                        type="password"
+                        name="confirmPassword"
+                        className="input mt-4"
+                        placeholder="Re-type your password"
+                      />
+                      <input
+                        className="btn btn-neutral mt-4"
+                        type="submit"
+                        value="Register"
+                      />
+                    </fieldset>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default SignUp;
